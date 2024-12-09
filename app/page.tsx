d2e2f8aebed0bedcecb10804dbf272e6d3a8c5a1
Daniel-627 <ochiengdaniel627@gmail.com>
2024-12-09 18:20:09 +0300

    Updating page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Companies from "@/components/Companies";
 import CreativeProcess from "@/components/CreativeProcess";
 import Hero from "@/components/Hero";
+import Overview from "@/components/Overview";
 import Services from "@/components/Services";
 import Works from "@/components/Works";
 import Image from "next/image";
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <div>
        <Hero />
+       <Overview />
        <Works />
        <Companies />
        <Services />
